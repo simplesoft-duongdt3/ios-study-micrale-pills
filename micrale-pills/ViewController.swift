79133b021a844615c3e1567980936e2486e5e28d
Dzoan Duong <doanthanhduong11031990@gmail.com>
2017-04-04 00:46:31 +0700

    add picker view

--- micrale-pills/ViewController.swift
+++ micrale-pills/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    let cites = ["HCM", "Ha Noi", "Hai Phong", "Ben Tre"]
+    @IBOutlet weak var btChooseCity: UIButton!
+    @IBOutlet weak var pickerCities: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerCities.dataSource = self
+        pickerCities.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btChooseCityClick(_ sender: Any) {
+        pickerCities.isHidden = false
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cites[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        btChooseCity.setTitle(cites[row], for: UIControlState())
+        pickerCities.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cites.count
+    }
 }
 
